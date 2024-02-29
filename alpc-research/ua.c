766d5d6986cdbd08fddc46c548ac13ce27e5e987
kanren3 <760917197@qq.com>
2024-02-29 22:15:06 +0800

    add ua

---
+++ alpc-research/ua.c
@@ -0,0 +1,531 @@
+﻿#include <ntstatus.h>
+#define WIN32_NO_STATUS
+#include <windows.h>
+#include <winternl.h>
+#include "ua.h"
+
+PVOID
+NTAPI
+UaAllocateZeroHeap (
+    IN SIZE_T Size
+)
+{
+    PVOID BaseAddress;
+
+    BaseAddress = malloc(Size);
+
+    if (NULL != BaseAddress) {
+        RtlZeroMemory(BaseAddress, Size);
+    }
+
+    return BaseAddress;
+}
+
+VOID
+NTAPI
+UaFreeHeap (
+    IN PVOID BaseAddress
+)
+{
+    free(BaseAddress);
+}
+
+VOID
+NTAPI
+UaInitializePortAttributes (
+    OUT PALPC_PORT_ATTRIBUTES PortAttributes
+)
+{
+    RtlZeroMemory(PortAttributes, sizeof(ALPC_PORT_ATTRIBUTES));
+
+    PortAttributes->MaxMessageLength = ALPC_MAX_ALLOWED_MESSAGE_LENGTH;
+}
+
+NTSTATUS
+NTAPI
+UaAlpcRequest (
+    IN PKA_SERVER Server
+)
+{
+    Server->OnRequest(Server->PortMessage);
+
+    NtAlpcSendWaitReceivePort(Server->ConnectionPortHandle,
+                              ALPC_MSGFLG_RELEASE_MESSAGE,
+                              Server->PortMessage,
+                              NULL,
+                              NULL,
+                              NULL,
+                              NULL,
+                              NULL);
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
+UaAlpcDatagram (
+    IN PKA_SERVER Server
+)
+{
+    PALPC_CONTEXT_ATTR ContextAttribute;
+
+    ContextAttribute = AlpcGetMessageAttribute(Server->MessageAttributes,
+                                               ALPC_MESSAGE_CONTEXT_ATTRIBUTE);
+
+    if (NULL == ContextAttribute) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    Server->OnDatagram(Server->PortMessage);
+
+    if (0 != (Server->PortMessage->u2.s2.Type & LPC_CONTINUATION_REQUIRED)) {
+        NtAlpcCancelMessage(Server->ConnectionPortHandle, 0, ContextAttribute);
+    }
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
+UaAlpcDisconnect (
+    IN PKA_SERVER Server
+)
+{
+    NTSTATUS Status;
+    PALPC_CONTEXT_ATTR ContextAttribute;
+    PPORT_CONTEXT PortContext;
+
+    ContextAttribute = AlpcGetMessageAttribute(Server->MessageAttributes,
+                                               ALPC_MESSAGE_CONTEXT_ATTRIBUTE);
+
+    if (NULL == ContextAttribute) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    PortContext = ContextAttribute->PortContext;
+
+    Status = NtClose(PortContext->CommunicationPortHandle);
+
+    UaFreeHeap(PortContext);
+
+    return Status;
+}
+
+NTSTATUS
+NTAPI
+UaAlpcConnect (
+    IN PKA_SERVER Server
+)
+{
+    NTSTATUS Status;
+    PPORT_CONTEXT PortContext;
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    HANDLE CommunicationPortHandle;
+
+    InitializeObjectAttributes(&ObjectAttributes,
+                               NULL,
+                               OBJ_CASE_INSENSITIVE,
+                               NULL,
+                               NULL);
+
+    if (FALSE != Server->OnConnect(Server->PortMessage)) {
+        PortContext = UaAllocateZeroHeap(sizeof(PORT_CONTEXT));
+
+        if (NULL != PortContext) {
+            PortContext->ClientId = Server->PortMessage->ClientId;
+
+            Status = NtAlpcAcceptConnectPort(&PortContext->CommunicationPortHandle,
+                                             Server->ConnectionPortHandle,
+                                             0,
+                                             &ObjectAttributes,
+                                             &Server->PortAttributes,
+                                             PortContext,
+                                             Server->PortMessage,
+                                             NULL,
+                                             TRUE);
+
+            if (FALSE == NT_SUCCESS (Status)) {
+                NtAlpcAcceptConnectPort(&CommunicationPortHandle,
+                                        Server->ConnectionPortHandle,
+                                        0,
+                                        &ObjectAttributes,
+                                        &Server->PortAttributes,
+                                        NULL,
+                                        Server->PortMessage,
+                                        NULL,
+                                        FALSE);
+
+                UaFreeHeap(PortContext);
+            }
+        }
+        else {
+            NtAlpcAcceptConnectPort(&CommunicationPortHandle,
+                                    Server->ConnectionPortHandle,
+                                    0,
+                                    &ObjectAttributes,
+                                    &Server->PortAttributes,
+                                    NULL,
+                                    Server->PortMessage,
+                                    NULL,
+                                    FALSE);
+        }
+    }
+    else {
+        NtAlpcAcceptConnectPort(&CommunicationPortHandle,
+                                Server->ConnectionPortHandle,
+                                0,
+                                &ObjectAttributes,
+                                &Server->PortAttributes,
+                                NULL,
+                                Server->PortMessage,
+                                NULL,
+                                FALSE);
+    }
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
+UaProcessMessage (
+    IN PKA_SERVER Server
+)
+{
+    NTSTATUS Status;
+
+    switch (LOBYTE(Server->PortMessage->u2.s2.Type)) {
+    case LPC_REQUEST:
+        Status = UaAlpcRequest(Server);
+        break;
+    case LPC_DATAGRAM:
+        Status = UaAlpcDatagram(Server);
+        break;
+    case LPC_PORT_CLOSED:
+        Status = UaAlpcDisconnect(Server);
+        break;
+    case LPC_CLIENT_DIED:
+        Status = UaAlpcDisconnect(Server);
+        break;
+    case LPC_CONNECTION_REQUEST:
+        Status = UaAlpcConnect(Server);
+        break;
+    default:
+        Status = STATUS_SUCCESS;
+        break;
+    }
+
+    return Status;
+}
+
+DWORD
+WINAPI
+UaServerWorker (
+    IN PKA_SERVER Server
+)
+{
+    NTSTATUS Status;
+    SIZE_T BufferLength;
+
+    while (TRUE) {
+        BufferLength = ALPC_MAX_ALLOWED_MESSAGE_LENGTH;
+
+        Status = NtAlpcSendWaitReceivePort(Server->ConnectionPortHandle,
+                                           0,
+                                           NULL,
+                                           NULL,
+                                           Server->PortMessage,
+                                           &BufferLength,
+                                           Server->MessageAttributes,
+                                           NULL);
+
+        if (FALSE == NT_SUCCESS(Status)) {
+            break;
+        }
+
+        Status = UaProcessMessage(Server);
+
+        if (FALSE == NT_SUCCESS(Status)) {
+            break;
+        }
+    }
+
+    return STATUS_SUCCESS;
+}
+
+VOID
+NTAPI
+UaTerminateServer (
+    IN PKA_SERVER Server
+)
+{
+    NtClose(Server->ConnectionPortHandle);
+    NtWaitForSingleObject(Server->ServerThreadHandle, FALSE, NULL);
+
+    UaFreeHeap(Server->PortMessage);
+    UaFreeHeap(Server->MessageAttributes);
+    UaFreeHeap(Server);
+}
+
+PKA_SERVER
+NTAPI
+UaCreateServer (
+    IN LPCWSTR PortName,
+    IN REQUEST_PROCEDURE OnRequest,
+    IN DATAGRAM_PROCEDURE OnDatagram,
+    IN CONNECTION_PROCEDURE OnConnect
+)
+{
+    NTSTATUS Status;
+    PKA_SERVER Server;
+    ULONG MessageAttributesMask;
+    SIZE_T MessageAttributesSize;
+    SIZE_T RequiredBufferSize;
+    OBJECT_ATTRIBUTES ObjectAttributes;
+
+    Server = UaAllocateZeroHeap(sizeof(KA_SERVER));
+
+    if (NULL == Server) {
+        goto Cleanup;
+    }
+
+    Server->OnRequest = OnRequest;
+    Server->OnDatagram = OnDatagram;
+    Server->OnConnect = OnConnect;
+
+    MessageAttributesMask = ALPC_MESSAGE_VIEW_ATTRIBUTE | \
+        ALPC_MESSAGE_CONTEXT_ATTRIBUTE;
+
+    MessageAttributesSize = AlpcGetHeaderSize(MessageAttributesMask);
+
+    Server->MessageAttributes = UaAllocateZeroHeap(MessageAttributesSize);
+
+    if (NULL == Server->MessageAttributes) {
+        goto Cleanup;
+    }
+
+    Status = AlpcInitializeMessageAttribute(MessageAttributesMask,
+                                            Server->MessageAttributes,
+                                            MessageAttributesSize,
+                                            &RequiredBufferSize);
+
+    if (FALSE == NT_SUCCESS(Status)) {
+        goto Cleanup;
+    }
+
+    Server->PortMessage = UaAllocateZeroHeap(ALPC_MAX_ALLOWED_MESSAGE_LENGTH);
+
+    if (NULL == Server->PortMessage) {
+        goto Cleanup;
+    }
+
+    UaInitializePortAttributes(&Server->PortAttributes);
+
+    RtlInitUnicodeString(&Server->PortName, PortName);
+
+    InitializeObjectAttributes(&ObjectAttributes,
+                               &Server->PortName,
+                               OBJ_CASE_INSENSITIVE,
+                               NULL,
+                               NULL);
+
+    Status = NtAlpcCreatePort(&Server->ConnectionPortHandle,
+                              &ObjectAttributes,
+                              &Server->PortAttributes);
+
+    if (FALSE == NT_SUCCESS(Status)) {
+        goto Cleanup;
+    }
+
+    Server->ServerThreadHandle = CreateThread(NULL, 0, UaServerWorker, Server, 0, NULL);
+
+    if (NULL == Server->ServerThreadHandle) {
+        goto Cleanup;
+    }
+
+    return Server;
+
+Cleanup:
+
+    if (NULL != Server) {
+        if (NULL != Server->ConnectionPortHandle) {
+            NtClose(Server->ConnectionPortHandle);
+        }
+
+        if (NULL != Server->MessageAttributes) {
+            UaFreeHeap(Server->MessageAttributes);
+        }
+
+        if (NULL != Server->PortMessage) {
+            UaFreeHeap(Server->PortMessage);
+        }
+
+        UaFreeHeap(Server);
+    }
+
+    return NULL;
+}
+
+NTSTATUS
+NTAPI
+UaSendSynchronousRequest (
+    IN HANDLE CommunicationPortHandle,
+    IN OUT PVOID RequestDataBuffer,
+    IN USHORT RequestDataLength
+)
+{
+    NTSTATUS Status;
+    USHORT TotalLength;
+    PPORT_MESSAGE PortMessage;
+    PVOID DataBuffer;
+    SIZE_T BufferLength;
+
+    TotalLength = sizeof(PORT_MESSAGE) + RequestDataLength;
+
+    if (TotalLength > ALPC_MAX_ALLOWED_MESSAGE_LENGTH) {
+        return STATUS_BUFFER_OVERFLOW;
+    }
+
+    PortMessage = UaAllocateZeroHeap(TotalLength);
+
+    if (NULL == PortMessage) {
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    PortMessage->u1.s1.TotalLength = TotalLength;
+    PortMessage->u1.s1.DataLength = RequestDataLength;
+
+    DataBuffer = PortMessage + 1;
+
+    RtlCopyMemory(DataBuffer, RequestDataBuffer, RequestDataLength);
+
+    BufferLength = TotalLength;
+
+    Status = NtAlpcSendWaitReceivePort(CommunicationPortHandle,
+                                       ALPC_MSGFLG_SYNC_REQUEST,
+                                       PortMessage,
+                                       NULL,
+                                       PortMessage,
+                                       &BufferLength,
+                                       NULL,
+                                       NULL);
+
+    RtlCopyMemory(RequestDataBuffer, DataBuffer, RequestDataLength);
+
+    UaFreeHeap(PortMessage);
+
+    return Status;
+}
+
+NTSTATUS
+NTAPI
+UaSendDatagram (
+    IN HANDLE CommunicationPortHandle,
+    IN PVOID DatagramBuffer,
+    IN USHORT DatagramLength
+)
+{
+    NTSTATUS Status;
+    USHORT TotalLength;
+    PPORT_MESSAGE PortMessage;
+    PVOID DataBuffer;
+
+    TotalLength = sizeof(PORT_MESSAGE) + DatagramLength;
+
+    if (TotalLength > ALPC_MAX_ALLOWED_MESSAGE_LENGTH) {
+        return STATUS_BUFFER_OVERFLOW;
+    }
+
+    PortMessage = UaAllocateZeroHeap(TotalLength);
+
+    if (NULL == PortMessage) {
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    PortMessage->u1.s1.TotalLength = TotalLength;
+    PortMessage->u1.s1.DataLength = DatagramLength;
+
+    DataBuffer = PortMessage + 1;
+
+    RtlCopyMemory(DataBuffer, DatagramBuffer, DatagramLength);
+
+    Status = NtAlpcSendWaitReceivePort(CommunicationPortHandle,
+                                       ALPC_MSGFLG_RELEASE_MESSAGE,
+                                       PortMessage,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL);
+
+    UaFreeHeap(PortMessage);
+
+    return Status;
+}
+
+VOID
+NTAPI
+UaDisconnectServer (
+    IN HANDLE CommunicationPortHandle
+)
+{
+    NtClose(CommunicationPortHandle);
+}
+
+NTSTATUS
+NTAPI
+UaConnectServer (
+    OUT PHANDLE CommunicationPortHandle,
+    IN LPCWSTR ServerPortName,
+    IN PVOID ConnectionDataBuffer OPTIONAL,
+    IN USHORT ConnectionDataLength
+)
+{
+    NTSTATUS Status;
+    SIZE_T BufferLength;
+    USHORT TotalLength;
+    PPORT_MESSAGE PortMessage;
+    PVOID DataBuffer;
+    ALPC_PORT_ATTRIBUTES PortAttributes;
+    UNICODE_STRING PortName;
+
+    TotalLength = sizeof(PORT_MESSAGE) + ConnectionDataLength;
+
+    if (TotalLength > ALPC_MAX_ALLOWED_MESSAGE_LENGTH) {
+        return STATUS_BUFFER_OVERFLOW;
+    }
+
+    PortMessage = UaAllocateZeroHeap(TotalLength);
+
+    if (NULL == PortMessage) {
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    PortMessage->u1.s1.TotalLength = TotalLength;
+    PortMessage->u1.s1.DataLength = ConnectionDataLength;
+
+    DataBuffer = PortMessage + 1;
+
+    RtlCopyMemory(DataBuffer, ConnectionDataBuffer, ConnectionDataLength);
+
+    UaInitializePortAttributes(&PortAttributes);
+
+    RtlInitUnicodeString(&PortName, ServerPortName);
+
+    BufferLength = ALPC_MAX_ALLOWED_MESSAGE_LENGTH;
+
+    Status = NtAlpcConnectPort(CommunicationPortHandle,
+                               &PortName,
+                               NULL,
+                               &PortAttributes,
+                               ALPC_MSGFLG_SYNC_REQUEST,
+                               NULL,
+                               PortMessage,
+                               &BufferLength,
+                               NULL,
+                               NULL,
+                               NULL);
+
+    UaFreeHeap(PortMessage);
+
+    return Status;
+}
---
+++ alpc-research/ua.h
@@ -0,0 +1,368 @@
+﻿#ifndef _UA_H_
+#define _UA_H_
+
+EXTERN_C_START
+
+#define ALPC_PORFLG_LPC_MODE 0x1000
+#define ALPC_PORFLG_ALLOW_IMPERSONATION 0x10000
+#define ALPC_PORFLG_ALLOW_LPC_REQUESTS 0x20000
+#define ALPC_PORFLG_WAITABLE_PORT 0x40000
+#define ALPC_PORFLG_ALLOW_DUP_OBJECT 0x80000
+#define ALPC_PORFLG_SYSTEM_PROCESS 0x100000
+#define ALPC_PORFLG_WAKE_POLICY1 0x200000
+#define ALPC_PORFLG_WAKE_POLICY2 0x400000
+#define ALPC_PORFLG_WAKE_POLICY3 0x800000
+#define ALPC_PORFLG_DIRECT_MESSAGE 0x1000000
+#define ALPC_PORFLG_ALLOW_MULTIHANDLE_ATTRIBUTE 0x2000000
+
+#define ALPC_PORFLG_OBJECT_TYPE_FILE 0x0001
+#define ALPC_PORFLG_OBJECT_TYPE_INVALID 0x0002
+#define ALPC_PORFLG_OBJECT_TYPE_THREAD 0x0004
+#define ALPC_PORFLG_OBJECT_TYPE_SEMAPHORE 0x0008
+#define ALPC_PORFLG_OBJECT_TYPE_EVENT 0x0010
+#define ALPC_PORFLG_OBJECT_TYPE_PROCESS 0X0020
+#define ALPC_PORFLG_OBJECT_TYPE_MUTEX 0x0040
+#define ALPC_PORFLG_OBJECT_TYPE_SECTION 0x0080
+#define ALPC_PORFLG_OBJECT_TYPE_REGKEY 0x0100
+#define ALPC_PORFLG_OBJECT_TYPE_TOKEN 0x0200
+#define ALPC_PORFLG_OBJECT_TYPE_COMPOSITION 0x0400
+#define ALPC_PORFLG_OBJECT_TYPE_JOB 0x0800
+#define ALPC_PORFLG_OBJECT_TYPE_ALL \
+    (ALPC_PORFLG_OBJECT_TYPE_FILE | ALPC_PORFLG_OBJECT_TYPE_THREAD | \
+     ALPC_PORFLG_OBJECT_TYPE_SEMAPHORE | ALPC_PORFLG_OBJECT_TYPE_EVENT | \
+     ALPC_PORFLG_OBJECT_TYPE_PROCESS | ALPC_PORFLG_OBJECT_TYPE_MUTEX | \
+     ALPC_PORFLG_OBJECT_TYPE_SECTION | ALPC_PORFLG_OBJECT_TYPE_REGKEY | \
+     ALPC_PORFLG_OBJECT_TYPE_TOKEN | ALPC_PORFLG_OBJECT_TYPE_COMPOSITION | \
+     ALPC_PORFLG_OBJECT_TYPE_JOB)
+
+#define LPC_REQUEST 1
+#define LPC_REPLY 2
+#define LPC_DATAGRAM 3
+#define LPC_LOST_REPLY 4
+#define LPC_PORT_CLOSED 5
+#define LPC_CLIENT_DIED 6
+#define LPC_EXCEPTION 7
+#define LPC_DEBUG_EVENT 8
+#define LPC_ERROR_EVENT 9
+#define LPC_CONNECTION_REQUEST 10
+#define LPC_CONTINUATION_REQUIRED 0x2000
+
+typedef char CCHAR;          // winnt
+typedef short CSHORT;
+typedef ULONG CLONG;
+
+typedef CCHAR *PCCHAR;
+typedef CSHORT *PCSHORT;
+typedef CLONG *PCLONG;
+
+typedef struct _PORT_MESSAGE {
+    union {
+        struct {
+            CSHORT DataLength;
+            CSHORT TotalLength;
+        } s1;
+        ULONG Length;
+    } u1;
+    union {
+        struct {
+            CSHORT Type;
+            CSHORT DataInfoOffset;
+        } s2;
+        ULONG ZeroInit;
+    } u2;
+    union {
+        CLIENT_ID ClientId;
+        double DoNotUseThisField;
+    };
+    ULONG MessageId;
+    union {
+        SIZE_T ClientViewSize;
+        ULONG CallbackId;
+    };
+} PORT_MESSAGE, *PPORT_MESSAGE;
+
+typedef struct _ALPC_PORT_ATTRIBUTES {
+    ULONG Flags;
+    SECURITY_QUALITY_OF_SERVICE SecurityQos;
+    SIZE_T MaxMessageLength;
+    SIZE_T MemoryBandwidth;
+    SIZE_T MaxPoolUsage;
+    SIZE_T MaxSectionSize;
+    SIZE_T MaxViewSize;
+    SIZE_T MaxTotalSectionSize;
+    ULONG DupObjectTypes;
+#ifdef _WIN64
+    ULONG Reserved;
+#endif
+} ALPC_PORT_ATTRIBUTES, *PALPC_PORT_ATTRIBUTES;
+
+typedef struct _ALPC_MESSAGE_ATTRIBUTES {
+    ULONG AllocatedAttributes;
+    ULONG ValidAttributes;
+} ALPC_MESSAGE_ATTRIBUTES, *PALPC_MESSAGE_ATTRIBUTES;
+
+#define ALPC_MESSAGE_SECURITY_ATTRIBUTE 0x80000000
+#define ALPC_MESSAGE_VIEW_ATTRIBUTE 0x40000000
+#define ALPC_MESSAGE_CONTEXT_ATTRIBUTE 0x20000000
+#define ALPC_MESSAGE_HANDLE_ATTRIBUTE 0x10000000
+
+typedef struct _ALPC_CONTEXT_ATTR {
+    PVOID PortContext;
+    PVOID MessageContext;
+    ULONG Sequence;
+    ULONG MessageId;
+    ULONG CallbackId;
+} ALPC_CONTEXT_ATTR, *PALPC_CONTEXT_ATTR;
+
+#define ALPC_MSGFLG_REPLY_MESSAGE 0x1
+#define ALPC_MSGFLG_LPC_MODE 0x2
+#define ALPC_MSGFLG_RELEASE_MESSAGE 0x10000
+#define ALPC_MSGFLG_SYNC_REQUEST 0x20000
+#define ALPC_MSGFLG_TRACK_PORT_REFERENCES 0x40000
+#define ALPC_MSGFLG_WAIT_USER_MODE 0x100000
+#define ALPC_MSGFLG_WAIT_ALERTABLE 0x200000
+#define ALPC_MSGFLG_WOW64_CALL 0x80000000
+
+NTSYSAPI
+PVOID
+NTAPI
+AlpcGetMessageAttribute(
+    _In_ PALPC_MESSAGE_ATTRIBUTES Buffer,
+    _In_ ULONG AttributeFlag
+);
+
+NTSYSAPI
+ULONG
+NTAPI
+AlpcGetHeaderSize(
+    _In_ ULONG Flags
+);
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+AlpcInitializeMessageAttribute(
+    _In_ ULONG AttributeFlags,
+    _Out_opt_ PALPC_MESSAGE_ATTRIBUTES Buffer,
+    _In_ SIZE_T BufferSize,
+    _Out_ PSIZE_T RequiredBufferSize
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcSendWaitReceivePort(
+    _In_ HANDLE PortHandle,
+    _In_ ULONG Flags,
+    _In_reads_bytes_opt_(SendMessage->u1.s1.TotalLength) PPORT_MESSAGE SendMessage,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES SendMessageAttributes,
+    _Out_writes_bytes_to_opt_(*BufferLength, *BufferLength) PPORT_MESSAGE ReceiveMessage,
+    _Inout_opt_ PSIZE_T BufferLength,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES ReceiveMessageAttributes,
+    _In_opt_ PLARGE_INTEGER Timeout
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcSendWaitReceivePort(
+    _In_ HANDLE PortHandle,
+    _In_ ULONG Flags,
+    _In_reads_bytes_opt_(SendMessage->u1.s1.TotalLength) PPORT_MESSAGE SendMessage,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES SendMessageAttributes,
+    _Out_writes_bytes_to_opt_(*BufferLength, *BufferLength) PPORT_MESSAGE ReceiveMessage,
+    _Inout_opt_ PSIZE_T BufferLength,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES ReceiveMessageAttributes,
+    _In_opt_ PLARGE_INTEGER Timeout
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcCancelMessage(
+    _In_ HANDLE PortHandle,
+    _In_ ULONG Flags,
+    _In_ PALPC_CONTEXT_ATTR MessageContext
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcCancelMessage(
+    _In_ HANDLE PortHandle,
+    _In_ ULONG Flags,
+    _In_ PALPC_CONTEXT_ATTR MessageContext
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcAcceptConnectPort(
+    _Out_ PHANDLE PortHandle,
+    _In_ HANDLE ConnectionPortHandle,
+    _In_ ULONG Flags,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_opt_ PALPC_PORT_ATTRIBUTES PortAttributes,
+    _In_opt_ PVOID PortContext,
+    _In_reads_bytes_(ConnectionRequest->u1.s1.TotalLength) PPORT_MESSAGE ConnectionRequest,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES ConnectionMessageAttributes,
+    _In_ BOOLEAN AcceptConnection
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcAcceptConnectPort(
+    _Out_ PHANDLE PortHandle,
+    _In_ HANDLE ConnectionPortHandle,
+    _In_ ULONG Flags,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_opt_ PALPC_PORT_ATTRIBUTES PortAttributes,
+    _In_opt_ PVOID PortContext,
+    _In_reads_bytes_(ConnectionRequest->u1.s1.TotalLength) PPORT_MESSAGE ConnectionRequest,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES ConnectionMessageAttributes,
+    _In_ BOOLEAN AcceptConnection
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcCreatePort(
+    _Out_ PHANDLE PortHandle,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_opt_ PALPC_PORT_ATTRIBUTES PortAttributes
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcCreatePort(
+    _Out_ PHANDLE PortHandle,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_opt_ PALPC_PORT_ATTRIBUTES PortAttributes
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcConnectPort(
+    _Out_ PHANDLE PortHandle,
+    _In_ PUNICODE_STRING PortName,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_opt_ PALPC_PORT_ATTRIBUTES PortAttributes,
+    _In_ ULONG Flags,
+    _In_opt_ PSID RequiredServerSid,
+    _Inout_updates_bytes_to_opt_(*BufferLength, *BufferLength) PPORT_MESSAGE ConnectionMessage,
+    _Inout_opt_ PSIZE_T BufferLength,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES OutMessageAttributes,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES InMessageAttributes,
+    _In_opt_ PLARGE_INTEGER Timeout
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAlpcConnectPort(
+    _Out_ PHANDLE PortHandle,
+    _In_ PUNICODE_STRING PortName,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_opt_ PALPC_PORT_ATTRIBUTES PortAttributes,
+    _In_ ULONG Flags,
+    _In_opt_ PSID RequiredServerSid,
+    _Inout_updates_bytes_to_opt_(*BufferLength, *BufferLength) PPORT_MESSAGE ConnectionMessage,
+    _Inout_opt_ PSIZE_T BufferLength,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES OutMessageAttributes,
+    _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES InMessageAttributes,
+    _In_opt_ PLARGE_INTEGER Timeout
+);
+
+#ifndef _WIN64
+#define ALPC_MAX_ALLOWED_MESSAGE_LENGTH 0xFFEF
+#else
+#define ALPC_MAX_ALLOWED_MESSAGE_LENGTH 0xFFFF
+#endif
+
+typedef struct _PORT_CONTEXT {
+    CLIENT_ID ClientId;
+    HANDLE CommunicationPortHandle;
+} PORT_CONTEXT, *PPORT_CONTEXT;
+
+typedef
+VOID
+(NTAPI *REQUEST_PROCEDURE)(
+    IN OUT PPORT_MESSAGE PortMessage
+    );
+
+typedef
+VOID
+(NTAPI *DATAGRAM_PROCEDURE)(
+    IN PPORT_MESSAGE PortMessage
+    );
+
+typedef
+BOOLEAN
+(NTAPI *CONNECTION_PROCEDURE)(
+    IN PPORT_MESSAGE PortMessage
+    );
+
+typedef struct _KA_SERVER {
+    UNICODE_STRING PortName;
+    REQUEST_PROCEDURE OnRequest;
+    DATAGRAM_PROCEDURE OnDatagram;
+    CONNECTION_PROCEDURE OnConnect;
+    PPORT_MESSAGE PortMessage;
+    ALPC_PORT_ATTRIBUTES PortAttributes;
+    PALPC_MESSAGE_ATTRIBUTES MessageAttributes;
+    HANDLE ConnectionPortHandle;
+    HANDLE ServerThreadHandle;
+} KA_SERVER, *PKA_SERVER;
+
+VOID
+NTAPI
+UaTerminateServer (
+    IN PKA_SERVER ServerObject
+);
+
+PKA_SERVER
+NTAPI
+UaCreateServer (
+    IN LPCWSTR PortName,
+    IN REQUEST_PROCEDURE OnRequest,
+    IN DATAGRAM_PROCEDURE OnDatagram,
+    IN CONNECTION_PROCEDURE OnConnect
+);
+
+NTSTATUS
+NTAPI
+UaSendSynchronousRequest (
+    IN HANDLE CommunicationPortHandle,
+    IN OUT PVOID RequestDataBuffer,
+    IN USHORT RequestDataLength
+);
+
+NTSTATUS
+NTAPI
+UaSendDatagram (
+    IN HANDLE CommunicationPortHandle,
+    IN PVOID DatagramBuffer,
+    IN USHORT DatagramLength
+);
+
+VOID
+NTAPI
+UaDisconnectServer (
+    IN HANDLE CommunicationPortHandle
+);
+
+NTSTATUS
+NTAPI
+UaConnectServer (
+    OUT PHANDLE CommunicationPortHandle,
+    IN LPCWSTR PortName,
+    IN PVOID ConnectionDataBuffer OPTIONAL,
+    IN USHORT ConnectionDataLength
+);
+
+EXTERN_C_END
+
+#endif
